cf4d043a37afd9ffa771d2b4a67276b361ea991a
Arthaskj <1285707427@qq.com>
2017-10-24 19:38:16 +0800

    19:38 App模板

---
+++ APP/appDemo/suning/js/index.js
@@ -0,0 +1,15 @@
+
+window.onload=function(){
+    document.getElementById('top_search').addEventListener('tap', function() {
+        //打开关于页面
+        mui.openWindow({
+          url: 'search.html', 
+          id:'top_search'
+        });
+      });
+
+
+
+
+
+}
